394dec9b2ccb32e3bc01895aaf5ba7a71bc7fc1f
Dart_Mac <bboydart91@gmail.com>
2016-04-10 14:32:35 +0900

    texture upload functions were upgraded
    Former-commit-id: ea0ab290a9c114cd468097da04d9604c1ed232cc

--- plugin/lubycon_plugins/3dLab/test.js
+++ plugin/lubycon_plugins/3dLab/test.js
@@ -10,61 +10,82 @@ $(window).on("load",function(){
 			renderer.setSize(window.innerWidth, window.innerHeight);
 		},
 		btnTrigger: function(){
-			var data = $(this).data("target"),
-			target = $("#"+data);
-			switch(data){
-				case "objInput" : target.off("change").on("change",funcs.uploadObj); break;
-				case "texInput" : target.off("change").on("change",funcs.uploadTex); break;
+			var data = $(this).data("value"),
+			target = $(this).data("target"),
+			input = $("#"+target),
+			inputInit = input.removeClass();
+			switch(target){
+				case "objInput" : input.off("change").on("change",funcs.uploadObj); break;
+				case "texInput" : 
+					input.addClass(data); 
+					input.off("change").on("change",funcs.uploadTex);
+				break;
 				default : $.error("Can't find input element : " + target); break;
 			}
-			target.trigger("click");
+			input.trigger("click");
 		},
 		uploadObj: function(event){
 			var $input = $(this),
 			$object = event.target.files;
 			funcs.loaders($object[0]);
-			console.log("uploadObj");
+			//console.log("uploadObj");
 		},
 		uploadTex: function(event){
-			console.log("a");
-			var input = $(this);
+			var $input = $(this);
 			var $texture = event.target.files;
+			var type = $input.attr("class");
+			var material;
 			$.each($texture,function(i,file){
 				var reader = new FileReader();
 				reader.readAsDataURL(file);
 				reader.onload = function(event){
-					var material = new THREE.MeshPhongMaterial({
-	       				map: THREE.ImageUtils.loadTexture(event.target.result)
-	       			});
-	       			object.children[0].material = material;
+					switch(type){
+						case "diffuse" :
+			       			object.children[0].material.map = THREE.ImageUtils.loadTexture(event.target.result);
+			       			object.children[0].material.needsUpdate = true;
+		       			break;
+		       			case "specular" :
+		       				object.children[0].material.specularMap = THREE.ImageUtils.loadTexture(event.target.result);
+		       				object.children[0].material.needsUpdate = true;
+	       				break;
+	       				case "normal" : 
+	       					object.children[0].material.normalMap = THREE.ImageUtils.loadTexture(event.target.result);
+       						object.children[0].material.needsUpdate = true;
+   						break;
+	       				default: $.error("Texture load error"); break;
+					}
+					$input.removeClass();
 				}
 			});
 		},
 		loaders: function(file){
 			var reader = new FileReader();
 			var filename = file.name;
-			var ext = filename.split(".")[1];
+			var ext = filename.split(".").pop();
 			console.log(ext);
 			switch(ext){
 				case "obj" :
 					reader.addEventListener("load", function (event) {
 						var contents = event.target.result;
 						object = new THREE.OBJLoader().parse(contents);
+						var toJSON = object.toJSON();
+						//console.log(toJSON);
 						object.name = filename;
 						object.children[0].geometry.center();
 						object.children[0].material.color = new THREE.Color(0x888888);
 						object.children[0].material.specular = new THREE.Color(0xffffff);
 						object.children[0].material.shininess = 100;
 						object.children[0].material.shading = THREE.SmoothShading;
+						object.children[0].material.side = THREE.DoubleSide;
 						scene.add(object);
-						console.log(object.children[0].material);
 					},false);
 					reader.readAsText(file);
 				break;
 				case "fbx" :
 					reader.addEventListener("load", function(event){
 						var contents = event.target.result;
-						var object = new THREE.FBXLoader().parse(contents);
+						object = new THREE.FBXLoader().parse(contents);
+						var toJSON = object.toJSON();
 						object.name = filename;
 						object.children[0].geometry.center();
 						object.children[0].material.color = new THREE.Color(0x888888);
@@ -103,6 +124,7 @@ $(window).on("load",function(){
 		scene.add(dirLight,dirHelper,ambLight);
 
 		renderer = new THREE.WebGLRenderer();
+		renderer.setPixelRatio(2);
 		renderer.setSize(windowWidth, windowHeight);
 		renderer.setClearColor(0x222222, 1);
 		gl.appendChild(renderer.domElement);
@@ -122,10 +144,30 @@ $(window).on("load",function(){
 
 	function initUI(){
 		var btWrap = $("<div/>",{"class":"gl-bt-wrapper"}).appendTo("body"),
-		objUpload = $("<button/>",{"class":"gl-btn","html":"Upload Object","data-target":"objInput"}).appendTo(btWrap)
-		.on("click",funcs.btnTrigger),
-		texUpload = $("<button/>",{"class":"gl-btn","html":"Upload Texture","data-target":"texInput"}).appendTo(btWrap)
-		.on("click",funcs.btnTrigger);
+		objUpload = $("<button/>",{
+			"class":"gl-btn",
+			"html":"Upload Object",
+			"data-target":"objInput",
+			"data-value":"object"
+		}).appendTo(btWrap).on("click",funcs.btnTrigger),
+		diffuseUpload = $("<button/>",{
+			"class":"gl-btn",
+			"html":"Diffuse",
+			"data-target":"texInput",
+			"data-value":"diffuse"
+		}).appendTo(btWrap).on("click",funcs.btnTrigger),
+		specularUpload = $("<button/>",{
+			"class":"gl-btn",
+			"html":"Specular",
+			"data-target":"texInput",
+			"data-value":"specular"
+		}).appendTo(btWrap).on("click",funcs.btnTrigger),
+		normalUpload = $("<button/>",{
+			"class":"gl-btn",
+			"html":"Normal",
+			"data-target":"texInput",
+			"data-value":"normal"
+		}).appendTo(btWrap).on("click",funcs.btnTrigger);
 	}
 
 	function animate(){
