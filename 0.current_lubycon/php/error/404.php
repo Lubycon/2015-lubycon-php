ad93d54d4aed12c723435d64c53ad5d39262c3f9
Dart_Mac <bboydart91@gmail.com>
2016-05-11 22:54:33 +0900

    prototype of 404 page is uploaded
    Former-commit-id: 8acb847f00f961003409fc018f856234e523f3ae

--- 0.current_lubycon/php/error/404.php
+++ 0.current_lubycon/php/error/404.php
@@ -1,3 +1,69 @@
-<div>
-    <p>sorry! i can't found page ;(</p>
-</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta http-equiv="Pragma" content="no-cache" />
+    <meta name="subject" content="Lubycon" />
+    <meta name="description" content="free sources" />
+    <meta name="developed" content="Lubycon" />
+    <meta name="designed" content="Lubycon" />
+    <meta name="robots" content="index" />
+    <meta name="copyright" content="copyrights 2015 LUBYCON" />
+    <meta name="keywords" content="design, font ,vector, 3d design, community, designers, engineer, 3d printer, illustration, lubycon" />
+
+    <title>Lubycon</title>
+	
+	<link rel="shortcut icon" href="ch/img/logo/lubycon.ico" />  <!-- favicon -->
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" />
+	<link href='http://fonts.googleapis.com/css?family=Source Sans Pro:200,400' rel='stylesheet' type='text/css'>
+
+	<script type="text/javascript" src="js/core/jquery-1.12.2.min.js"></script> <!-- jquery library -->
+	<style>
+		* {
+			margin: 0;
+		}
+		body {
+			background: #282828;
+		}
+		#error-404 {
+		    position: fixed;
+		    top: 50%;
+		    margin-top: -350px;
+		    left: 50%;
+		    margin-left: -600px;
+		}
+		    #error-404 > img {
+		        width: 1200px;
+		        height: 700px;
+		    }
+	    #return_main {
+	    	position: absolute;
+	    	bottom: 30px;
+	    	left: 50%;
+	    	margin-left: -100px;
+	    	width: 200px;
+	    	height: 50px;
+	    	background: transparent;
+	    	border-radius: 5px;
+	    	border: 2px solid #ffffff;
+	    	font-size: 20px;
+	    	font-weight: 200;
+	    	color: #ffffff;
+	    	cursor: pointer;
+	    	outline: 0;
+	    	transition: border 0.2s, color 0.2s;
+	    }
+	    	#return_main:hover {
+	    		border: 2px solid #48cfad;
+	    		color: #48cfad;
+	    	}
+	</style>
+	<script>
+		function gotoPrevPage(){ window.history.back(); }
+	</script>
+</head>
+<body>
+	<div id="error-404"><img src="ch/img/404_page_image.svg" /></div>
+	<button id="return_main" onClick="gotoPrevPage()">Back</button>
+</body>
+</html>
