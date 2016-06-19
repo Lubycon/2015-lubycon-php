ebb472caa142ee68a26b03794230a909257e2547
kyungyuel noh <kyungyuel noh>
2016-06-19 19:24:57 +0900

    creator card was changed
    Former-commit-id: 2c0ccbe1bc3d5b93010e99fa12205f642b3556e1

--- lubycon/php/layout/creator_card.php
+++ lubycon/php/layout/creator_card.php
@@ -18,7 +18,7 @@
         $user_content3_num = $row['boardCode'];
     ?><!--you should change to mySQL later-->
     <div class="creators_card" data-index="<?=$usercode?>">
-        <div class="creator_info_wrap">
+        <!--<div class="creator_info_wrap">
             <div class="creator_top_info">
                 <div class="creator_pic">
                     <img src="<?=$user_img_url?>" alt="user_pic"/>
@@ -28,8 +28,8 @@
                 </div>
             </div>
             <div class="creator_mid_info">
-                <p class="creator_name"><a href="../personal_page/personal_page.php?cate=dashboard&usernum=<?=$usercode?>"><?=$username?></a></p><!--user name-->
-                <p class="creator_job"><?=$userjob?></p><!--job-->
+                <p class="creator_name"><a href="../personal_page/personal_page.php?cate=dashboard&usernum=<?=$usercode?>"><?=$username?></a></p>
+                <p class="creator_job"><?=$userjob?></p>
                 <p class="creator_location hidden-mb-b"><i class="fa fa-map-marker"></i><?=$usercity?>, <?=$usercountry?></p>
                 <article class="contents_count hidden-mb-b">
                     <p class="count_num"><?=$contents_count?></p>
@@ -55,13 +55,60 @@
                 </li>
                 <li class="usercontent">
                 <?php 
-                    echo "<a class='contents_link' href='../contents/contents_view.php?cate=artwork&conno=$user_content3_num'>";
+                    echo "<a class='cont ents_link' href='../contents/contents_view.php?cate=artwork&conno=$user_content3_num'>";
                 ?>
                     <img src="<?=$user_content3?>" alt="user_content"/>
                 </a>
                 </li>
             </ul>
-        </div>
-        
+        </div>-->
+        <div class="creator_card_header">
+            <span class="card_label">Creator of The <span class="this_month">September</span></span>
+        </div><!--header-->
+        <div class="creator_card_body">
+            <div class="creator_pic_wrap">
+                <div class="creator_pic"><img src="<?=$user_img_url?>" alt="user_pic"></div>
+                <div class="creator_location_pic"><img src="<?=$user_location_img?>" alt="user_location"></div>
+            </div>
+            <div class="creator_info_wrap">
+                <p class="creator_name"><a href="../personal_page/personal_page.php?cate=dashboard&usernum=<?=$usercode?>"><?=$username?></a></p>
+                <p class="creator_job"></i><?=$userjob?></p>
+                <p class="creator_location hidden-mb-b"><i class="fa fa-map-marker"></i><?=$usercity?>, <?=$usercountry?></p>
+                <article class="contents_count hidden-mb-b">
+                    <p class="contents_num"><?=$contents_count?></p>
+                    Contents
+                </article>
+            </div>
+        </div><!--body-->
+        <div class="creator_card_medal">
+            <ul>
+                <li></li>
+            </ul>
+        </div><!--medals-->
+        <div class="creator_card_footer">
+            <ul>
+                <li class="usercontent" data-value="0">
+                    <?php 
+                        echo "<a class='contents_link' href='../contents/contents_view.php?cate=artwork&conno=$user_content1_num'>";
+                    ?>
+                    <img src="<?=$user_content1?>" alt="user_content">
+                    </a>
+                </li>
+                <li class="usercontent" data-value="1">
+                    <?php 
+                        echo "<a class='contents_link' href='../contents/contents_view.php?cate=artwork&conno=$user_content2_num'>";
+                    ?>
+                    <img src="<?=$user_content2?>" alt="user_content">
+                    </a>
+                </li>
+                <li class="usercontent" data-value="2">
+                    <?php 
+                        echo "<a class='contents_link' href='../contents/contents_view.php?cate=artwork&conno=$user_content3_num'>";
+                    ?>
+                    <img src="<?=$user_content3?>" alt="user_content">
+                    </a>
+                </li>
+            </ul>
+        </div><!--footer-->
     </div>
 </li>
