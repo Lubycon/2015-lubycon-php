d4dbdd29d08e00aae36316b728f5a6c61bb4108c
Dart_Mac <bboydart91@gmail.com>
2016-07-01 23:21:16 +0900

    #0. main slider init method is move to ui.js #1. design of about_us is changed #2. border-radius / box-shadow are removed of lubySelector
    Former-commit-id: cd996f09e80fb20a3067eda25fefbf1c49bd6241

--- lubycon/index_body_mobile.php
+++ lubycon/index_body_mobile.php
@@ -4,5 +4,46 @@
     ?>
     <section class="mb-section">
         <!-- WORK -->
+        <div id="test">
+        	<span class="list"></span>
+        	<span class="list"></span>
+        	<span class="list"></span>
+        	<span class="list"></span>
+        	<span class="list"></span>
+        	<span class="list"></span>
+        	<span class="list"></span>
+        	<span class="list"></span>
+        	<span class="list"></span>
+        	<span class="list"></span>
+        	<span class="list"></span>
+        	<span class="list"></span>
+        	<span class="list"></span>
+        	<span class="list"></span>
+        	<span class="list"></span>
+        	<span class="list"></span>
+        	<span class="list"></span>
+        	<span class="list"></span>
+        	<span class="list"></span>
+        	<span class="list"></span>
+        </div>
+        <style>
+        	#test {
+        		width: 100%;
+        		margin-top: 100px;
+				overflow-y: hidden;
+			    overflow-x: scroll; 
+			    -webkit-overflow-scrolling:touch;
+			    white-space: nowrap;
+        	}
+        		#test .list {
+        			display: inline-block;
+        			width: 50px;
+					height: 50px;
+					display: inline-block;
+					margin: 0px 10px;
+					background: #222222;
+        		}
+
+        </style>
     </section>
 </section>
--- lubycon/js/core/ui.js
+++ lubycon/js/core/ui.js
@@ -1,7 +1,7 @@
 // TITLE : ui.js
 // For UI init
 
-$(function(){
+$(document).ready(function(){
     initUIComponents();
 
     function initUIComponents(){
@@ -34,6 +34,7 @@ $(function(){
             initGoTopBt();
         }
         else{
+            initMainSlider();
             contentCardHoverAction();
             contentsNameStickyAction();
         }
@@ -321,6 +322,59 @@ $(function(){
                 $this.parent(".modal").stop().fadeOut(200);
             }
         }
+    }
+
+    function initMainSlider(){
+        if($(".main-slider-wrapper").length === 0) return false; // EXCEPTION
+
+        var _SlideshowTransitions = [{ $Duration: 1200, $Opacity: 2 }]; //Fade
+        var options = {
+            $AutoPlay: true,                             //[Optional] Whether to auto play, to enable slideshow, this option must be set to true, default value is false
+            $AutoPlaySteps: 1,                           //[Optional] Steps to go for each navigation request (this options applys only when slideshow disabled), the default value is 1
+            $AutoPlayInterval: 3000,                     //[Optional] Interval (in milliseconds) to go for next slide since the previous stopped if the slider is auto playing, default value is 3000
+            $PauseOnHover: 1,                            //[Optional] Whether to pause when mouse over if a slider is auto playing, 0 no pause, 1 pause for desktop, 2 pause for touch device, 3 pause for desktop and touch device, 4 freeze for desktop, 8 freeze for touch device, 12 freeze for desktop and touch device, default value is 1
+            $ArrowKeyNavigation: true,                   //[Optional] Allows keyboard (arrow key) navigation or not, default value is false
+            $SlideDuration: 500,                         //[Optional] Specifies default duration (swipe) for slide in milliseconds, default value is 500
+            //$SlideWidth: 600,                          //[Optional] Width of every slide in pixels, default value is width of 'slides' container
+            //$SlideHeight: 300,                         //[Optional] Height of every slide in pixels, default value is height of 'slides' container
+            $SlideSpacing: 0,                            //[Optional] Space between each slide in pixels, default value is 0
+            $DisplayPieces: 1,                           //[Optional] Number of pieces to display (the slideshow would be disabled if the value is set to greater than 1), the default value is 1
+            $ParkingPosition: 0,
+            $DragOrientation: 0, //[Optional] The offset position to park slide (this options applys only when slideshow disabled), default value is 0.
+
+            $BulletNavigatorOptions: {                   //[Optional] Options to specify and enable navigator or not
+                $Class: $JssorBulletNavigator$,          //[Required] Class to create navigator instance
+                $ChanceToShow: 2,                        //[Required] 0 Never, 1 Mouse Over, 2 Always
+                $AutoCenter: 1,                          //[Optional] Auto center navigator in parent container, 0 None, 1 Horizontal, 2 Vertical, 3 Both, default value is 0
+                $Steps: 1,                               //[Optional] Steps to go for each navigation request, default value is 1
+                $Lanes: 1,                               //[Optional] Specify lanes to arrange items, default value is 1
+                $SpacingX: 10,                           //[Optional] Horizontal space between each item in pixel, default value is 0
+                $SpacingY: 10,                           //[Optional] Vertical space between each item in pixel, default value is 0
+                $Orientation: 1                          //[Optional] The orientation of the navigator, 1 horizontal, 2 vertical, default value is 1
+            },
+            $ArrowNavigatorOptions: {
+                $Class: $JssorArrowNavigator$,           //[Requried] Class to create arrow navigator instance
+                $ChanceToShow: 2,                        //[Required] 0 Never, 1 Mouse Over, 2 Always
+                $Steps: 1                                //[Optional] Steps to go for each navigation request, default value is 1
+            }
+        };
+
+        var jssor_slider1 = new $JssorSlider$("slider1", options);
+        var jssor_slider2 = new $JssorSlider$("slider2", options);
+        var jssor_slider3 = new $JssorSlider$("slider3", options);
+        //responsive code begin
+        //you can remove responsive code if you don't want the slider scales while window resizes
+        function ScaleSlider() {
+            var parentWidth = jssor_slider1.$Elmt.parentNode.clientWidth;
+            if (parentWidth)
+                jssor_slider1.$ScaleWidth(Math.min(parentWidth, 980));
+            else
+                window.setTimeout(ScaleSlider, 30);
+        }
+        ScaleSlider();
+        $(window).bind("load", ScaleSlider);
+        $(window).bind("resize", ScaleSlider);
+        $(window).bind("orientationchange", ScaleSlider);
     }
 
     function mainSliderRadioAction(){
