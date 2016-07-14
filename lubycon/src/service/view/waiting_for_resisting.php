0c6a9e25d6ab2341d4ff93c7ac7e7805fbd481ce
SsaRu <bihong05@hanmail.net>
2016-07-15 01:02:45 +0900

    #1. Edit Align Waitting_for_resist.php
    Former-commit-id: 75a7769fa0b38ba1f83b4a195f7fa1fc564acb58

--- lubycon/src/service/view/waiting_for_resisting.php
+++ lubycon/src/service/view/waiting_for_resisting.php
@@ -1,33 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <meta http-equiv="Pragma" content="no-cache" />
-    <meta name="subject" content="Lubycon" />
-    <meta name="description" content="free sources" />
-    <meta name="developed" content="Lubycon" />
-    <meta name="designed" content="Lubycon" />
-    <meta name="robots" content="index" />
-    <meta name="copyright" content="copyrights 2015 LUBYCON" />
-    <meta name="keywords" content="design, font ,vector, 3d design, community, designers, engineer, 3d printer, illustration, lubycon" />
-
-    <title>Lubycon</title>
-	
-	<link rel="shortcut icon" href="../asset/img/logo/lubycon.ico" />  <!-- favicon -->
-	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" />
-	<link rel='stylesheet' href='http://fonts.googleapis.com/css?family=Source Sans Pro:200,400' />
-	<link rel="stylesheet" href="../plugin/lib/animate.css" />
 	<link rel="stylesheet" href="./service/view/messagePage.css" />
-
-	<script type="text/javascript" src="../plugin/lib/jquery-1.12.2.min.js"></script> <!-- jquery library -->
-
-	<!-- doesn't have Core.js file --------------------------------------------------------- -->
-    <!-- <script type="text/javascript" src="../../js/core/core.js"></script> -->
-    <!-- ----------------------------------------------------------------------------------- -->
-
-    <script type="text/javascript" src="./messagePage.js"></script>
+    <script type="text/javascript" src="./service/view/messagePage.js"></script>
 </head>
-<body ondragstart="return false" onselectstart="return false">
 	
 	<?php
 		if(!isset($_SESSION['lubycon_nick']))
@@ -61,5 +37,4 @@
 			<div class="btn other-bt" data-value="resend" href="./pages/controller/sign_up/reCertifiEmail.php">Resend</div>
 		</div>
 	</section>
-</body>
 </html>
