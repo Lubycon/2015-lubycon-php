50b290144b49b57e847b0f0c3351602107a4c832
SsaRu <bihong05@hanmail.net>
2016-07-23 13:56:34 +0900

    #1. roll back session class
    Former-commit-id: 39534aa2ee2e4b15186f86efd7ee5a4e16b22d9b

--- lubycon/src/common/Class/session_class.php
+++ lubycon/src/common/Class/session_class.php
@@ -34,10 +34,10 @@ class Session{
 		if(isset($_SESSION) === true){
 			foreach($_SESSION as $name=>$val){
 				if(strpos($name,$this->seperator) !== false){
-					
-					if(isset($val) === true){
-					$count = $count + 1;	
-					
+					if(strpos($name,'session_id') == false){
+						if(isset($val) === true){
+						$count = $count + 1;
+						}	
 					}
 				}
 			}
@@ -52,19 +52,62 @@ class Session{
 	public function WriteSession($seperator="lubycon",$array){
 		$this->seperator = $seperator;
 
+		$_SESSION[$seperator.'_session_id'] = session_id();
+
 		foreach($array as $key=>$value){
 			if($key !== "pass")
 				$_SESSION[$seperator.'_'.$key] = $value;
 		}
+		
+		/*
+		$temp_sessionId = $seperator.'_session_id';
+		$temp_id = $seperator.'_id';
+		$temp_nick = $seperator.'_nick';
+		$temp_code = $seperator.'_code';
+		$temp_validation = $seperator.'_validation';
+
+		$_SESSION[$temp_sessionId] = $this->session_id = session_id();
+		$_SESSION[$temp_id] = $this->user_id = $id;
+		$_SESSION[$temp_nick] = $this->user_nick = $nick;
+		$_SESSION[$temp_code] = $this->user_code = $code;
+		*/
 	}
 
 	public function DestroySession(){
 		session_destroy();
 
-		foreach($_SESSION as $key=>$value){
-			$_SESSION[$key] = NULL;
-		}
+		$temp_sessionId = $this->seperator.'_session_id';
+		$temp_id = $this->seperator.'_id';
+		$temp_nick = $this->seperator.'_nick';
+		$temp_code = $this->seperator.'_code';
+
+		$_SESSION[$temp_sessionId] = NULL;
+		$_SESSION[$temp_id] = NULL;
+		$_SESSION[$temp_nick] = NULL;
+		$_SESSION[$temp_code] = NULL;
 	}
+
+	public function GetVar(){
+		$var = array();
+		foreach($_SESSION as $name=>$val){
+			if(strpos($name, $this->seperator) !== false){ // 아이덴티티 분리 후 검사해야할듯 재검토 필요 160307
+				$temp = array($name=>$val);
+				$var = array_merge($var,$temp);
+			}
+		}
+		return $var;
+	}
+
+	public function FreeResource(){
+		$this->session_id = null;
+		$this->user_id = null;
+		$this->user_nick = null;
+		$this->user_code = null;
+		$this->seperator = null;
+	}
+
+	public function GetSessionId(){return session_id();}
+	public function GetSessionName(){return session_name();}
 }
 
 ?>
