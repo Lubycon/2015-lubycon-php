26fa5b05b9291af6c29ccf6b5528925ed61c8fb3
Dart_DeskTop <bboydart91@gmail.com>
2016-07-28 23:30:01 +0900

    sign in page is fixed
    Former-commit-id: 48f170b4505fab7b2ae8277144bea7952326ad83

--- lubycon/src/pages/view/sign_in/login_page.php
+++ lubycon/src/pages/view/sign_in/login_page.php
@@ -1,5 +1,5 @@
 <link href="./pages/view/sign_in/login_page.css" rel="stylesheet" type="text/css" />
-<script src="./pages/view/sign_in/login_page.js"></script>
+<script src="./pages/controller/sign_in/login_page_renderer.js"></script>
 <div id="loginWrap">
     <div id="login_box">
         <header id="intro_wrap">
@@ -7,13 +7,11 @@
                 <figure id="logo_lubycon"></figure>
             </a>
         </header>
-        <form id="main_login" name="main_login" method="post" action="./pages/controller/sign_in/sign_in.php">
-            <div id="login_input">
-                    <input type="text" id="login_id" name="login_id" value="E-mail"/><i id="email_icon" class="fa fa-user"></i>
-                    <input type="password" id="login_pass" name="login_pass" value="Password" /><i id="pass_icon" class="fa fa-key"></i>
-            </div> <!-- end login_input div -->
-            <button type="submit" id="login_lubycon" class="animate_width"><i class="fa fa-unlock-alt"></i></button><!--submit bt-->
-        </form><!--end login_input form-->
+        <div id="login_input">
+                <input type="text" id="login_id" name="login_id" value="E-mail"/><i id="email_icon" class="fa fa-user"></i>
+                <input type="password" id="login_pass" name="login_pass" value="Password" /><i id="pass_icon" class="fa fa-key"></i>
+        </div> <!-- end login_input div -->
+        <button id="login_lubycon" class="animate_width"><i class="fa fa-unlock-alt"></i></button><!--submit bt-->
         <p id="create_acc">Create An Account</p>
         <a href="./index.php?dir=pages/view/sign_in/find_password" target="_self"><p id="forgot_pass">Forgot your password?</p></a>
     </div>  <!-- end login_box div -->
