30d1abc486c3f6be75f37d8ee540e5d4105aae27
Dart_DeskTop <bboydart91@gmail.com>
2016-08-03 23:49:49 +0900

    user authentication action is added
    Former-commit-id: b4332196a4bef92aa59a6ba36d4cef0346180e4d

--- lubycon/src/api/Authentication.module.js
+++ lubycon/src/api/Authentication.module.js
@@ -4,6 +4,10 @@ $(document).ready(function(){
     });
     function init(session){
         if(session.LoginState){
+            console.log("Authentication is loaded");
+
+            userActivity(session.MemberActivity);
+
             $(".signin_class").remove();
             $(".after_signin_class").show();
             console.log(session);
@@ -13,5 +17,16 @@ $(document).ready(function(){
             $(".signin_class").show();
             $(".after_signin_class").remove();
         }
+    }
+
+    function userActivity(activity){
+        // activity = 'inactive'; // TESTING...
+        if(activity === 'inactive' && getUrlParameter('dir') !== 'service/view/waiting_for_resisting'){
+            location.href = '?dir=service/view/waiting_for_resisting';
+        }
+        else if(activity === 'drop'){
+            alert("ERROR : Droped member");
+        }
+        else return false;
     }
 });
